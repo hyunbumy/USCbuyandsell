1a72ad909a4d1567e021e463a89724755d6cbd59
Hyun-Bum Yang <hyunbumy@usc.edu>
2017-07-23 23:08:24 -0700

    Make a Login servlet

---
+++ src/buyAndSell/Login.java
@@ -0,0 +1,25 @@
+package buyAndSell;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		// TODO: Check the DB for username and HASHED password (need a hash function)
+		
+	}
+
+}
