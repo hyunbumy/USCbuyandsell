17eaf96f83a9bd9c4759bab0a3310270e8448eb5
Brandon Holden <bholden@usc.edu>
2017-07-25 19:53:49 -0700

    can add item and keyword to db

--- src/buyAndSell/StoreDatabase.java
+++ src/buyAndSell/StoreDatabase.java
@@ -16,7 +16,7 @@ import com.mysql.jdbc.Statement;
 public class StoreDatabase {
 	
 	public static void main(String[] args) {
-		createUser("jmiller", "password", "jeff", "miller", "jmiller@usc.edu", "98498", "image");
+		//createUser("jmiller", "password", "jeff", "miller", "jmiller@usc.edu", "98498", "image");
 		login("jmiller", "password");
 		Item i = new Item ("3 piece suit", (float) 99.99, Category.ELECTRONIC, 1);
 		sellItem(i);
@@ -51,18 +51,14 @@ public class StoreDatabase {
 		//hash password
 		password = Hashing.sha256().hashString(password+"salt", StandardCharsets.UTF_8).toString();
 		
-		System.out.println("login");
-		
 		//query db
 		Statement st = connect();
 		ResultSet rs;
 		try {
 			rs = st.executeQuery("SELECT uname, pword, fname,lname,email,phoneNum,image,userID FROM UserTable"
 					+ " WHERE " + "uname=\'"+username+"\' AND pword=\'"+password+"\';");
-			System.out.println("login2");
 			// There should be either one or no result row
 			if(rs.next()) {
-				System.out.println("login3");
 				// If true, there was a match, therefore correct login
 				String uname = rs.getString("uname");
 				String pword = rs.getString("pword");
@@ -72,7 +68,6 @@ public class StoreDatabase {
 				String phoneNum = rs.getString("phoneNum");
 				String image = rs.getString("image");
 				int userID = rs.getInt("userID");
-				System.out.println(userID);
 				
 //				System.out.println("Username: "+ uname);
 //				System.out.println("Password: "+ pword);
@@ -85,7 +80,6 @@ public class StoreDatabase {
 				
 				User u = new User(fname, lname, email, phoneNum, uname, userID);
 				StoreDatabase.currUser = u;
-				System.out.println("set currUser");
 				return true;
 			}
 		} catch (SQLException e) {
@@ -160,13 +154,11 @@ public class StoreDatabase {
 		return false;
 	}
 	
-	
+	//add Item to ItemsTable, and keywords to Keyword table
 	public static void sellItem(Item i) {
-		//add Item to ItemsTable
 		//query db
 		Statement st = connect();
 		ResultSet rs;
-		System.out.println("sell item");
 		int sellingUserID = StoreDatabase.currUser.getUserID();
 		try {
 			String query = "INSERT INTO ItemsTable(sellingUser,title,price,category,quantity,description,image)\n";
@@ -174,85 +166,65 @@ public class StoreDatabase {
 					+"\'," + i.getQuantity() + "," + "\'"+i.getDescription()+"\'," +"\'"+ i.getImage() +"\');";
 			st.executeUpdate(query);
 			
-
+			//get the itemID that was generated
+			rs = st.executeQuery("SELECT itemID FROM ItemsTable WHERE " + "title=\'"+i.getName()+
+					"\' AND sellingUser="+sellingUserID+ " AND description=\'"+i.getDescription()+"\';");
+			
+			if (rs.next()) {
+				int itemID = rs.getInt("itemID");
+				//get the keywords and add those to db
+				Vector<String> keywords = new Vector<String>();
+				parseKeywords(i.getName(), keywords);
+				parseKeywords(i.getDescription(), keywords);
+				for (int j = 0; j < keywords.size(); j++) {
+					//add to Keywords
+					String k = keywords.get(j);
+					String dbStr = "INSERT INTO KeywordTable(keyword,itemID)\n";
+					dbStr += "VALUES (\'"+ k + "\'," + itemID +");";
+					st.executeUpdate(dbStr);
+				}
+			}
+			
 		} catch (SQLException e) {
-			System.out.println("Login failure: " + e.getMessage());
-		
-		}	
-		
-		
-		
-		//NEED THIS TO RETURN THE KEYWORDS	
-		//or I could just add to the database from there idk yet
-		parseKeywords(i);
-		
-		//add item to ItemTable and keywords to KeywordTable
+			System.out.println("Sell item failure: " + e.getMessage());
+		}			
 		
 	}
 	
 	
-	//parses the keywords of an item and adds them to the map (database)
-	private static void parseKeywords(Item item) {
-		//get keywords from the title and description
-		String name = item.getName();
-		String description = item.getDescription();
-		
-		//parse the name and optional description
-		parseStringAndAddToKeywords(name, item);
-		parseStringAndAddToKeywords(description, item);
-		
-	}
-	
-	private static void parseStringAndAddToKeywords(String toParse, Item item) {
-		//KEY NEEDS TO BE LOWERCASE TO MATCH SEARCH
-		
+	private static Vector<String> parseKeywords(String toParse, Vector<String> keywords) {	
 		if (toParse == null) {
-			return;
+			return keywords;
 		}
 		
 		//go through the name, make keyword strings splitting at every non-alpha char, add to map
-//		String keyword = "";
-//		for (int i = 0; i < toParse.length(); i++) {	
-//			if (Character.isLetter(toParse.charAt(i))) {
-//				keyword += toParse.charAt(i);
-//			}
-//			else {
-//				keyword = keyword.toLowerCase();
-//				//see if keyword is in the map
-//				if (keywordMap.containsKey(keyword)) {
-//					Vector<Item> currentItems = keywordMap.get(keyword);
-//					currentItems.add(item);
-//					keywordMap.put(keyword, currentItems);
-//				}
-//				else {
-//					Vector<Item> newItems = new Vector<Item>();
-//					newItems.add(item);
-//					keywordMap.put(keyword, newItems);
-//				}
-//				keyword = "";
-//			}
-//			
-//			//not recognizing the newline character- manual override to add it
-//			if (i == toParse.length()-1) {
-//				keyword = keyword.toLowerCase();
-//				//see if keyword is in the map
-//				if (keywordMap.containsKey(keyword)) {
-//					Vector<Item> currentItems = keywordMap.get(keyword);
-//					currentItems.add(item);
-//					keywordMap.put(keyword, currentItems);
-//				}
-//				else {
-//					Vector<Item> newItems = new Vector<Item>();
-//					newItems.add(item);
-//					keywordMap.put(keyword, newItems);
-//				}
-//			}
-//		}
+		String keyword = "";
+		for (int i = 0; i < toParse.length(); i++) {	
+			if ((Character.isLetter(toParse.charAt(i))) || (Character.isDigit(toParse.charAt(i)))) {
+				keyword += toParse.charAt(i);
+			}
+			else {
+				//means have a keyword, add to vector and reset
+				if (!(keyword.equals(""))) {
+					keywords.add(keyword);
+					keyword = "";
+				}
+			}	
+			//not recognizing the newline character- manual override to add it
+			if (i == toParse.length()-1) {
+				if (!(keyword.equals(""))) {
+					keywords.add(keyword);
+					keyword = "";
+				}
+			}
+		}
+		return keywords;
 	}
 	
 	
 	//search terms must be converted to lower case
 	public static Vector<Item> search(String searchTerm) {
+		//convert toLower for search terms and db keywords
 		Vector<Item> results = new Vector<Item>();
 		
 		String[] searchTerms = searchTerm.split(" ");
