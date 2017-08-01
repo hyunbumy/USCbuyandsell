6fce654ba957757396352d8ca9c8da714609bfd5
Brandon Holden <bholden@usc.edu>
2017-08-01 12:00:49 -0700

    fixing messages constuctors

--- src/buyAndSell/InterestMessage.java
+++ src/buyAndSell/InterestMessage.java
@@ -2,12 +2,8 @@ package buyAndSell;
 
 public class InterestMessage extends Message {
 
-	public InterestMessage(Item item) {
-		super();
-		this.item = item;
-		this.title = "Someone is interested in your "+ item.getName();
-		this.msg = StoreDatabase.getCurrUser().getfName() + " added your " + item.getName()
-				+ " to their Wishlist!";
+	public InterestMessage(Item item, String time, String date) {
+		super(item,time,date);
 	}
 	
 }
--- src/buyAndSell/RatingMessage.java
+++ src/buyAndSell/RatingMessage.java
@@ -1,5 +1,9 @@
 package buyAndSell;
 
 public class RatingMessage extends Message {
+	
+	public RatingMessage(Item item, String time, String date) {
+		super(item,time,date);
+	}
 
 }
